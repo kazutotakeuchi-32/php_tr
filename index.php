d025df5dbd77f62950af1d895d523034544053ad
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2022-04-03 21:07:30 +0900

    UsersContoroller::render 成功

--- index.php
+++ index.php
@@ -2,11 +2,16 @@
 
 require_once "vendor/autoload.php";
 require_once "config/app.php";
+require_once "controller/usersController.php";
 
 $app = new App();
 
 $app->router->get('/', function () {
   return 'Hello, world';
+});
+
+$app->router->get('/users', function(){
+  UsersContoroller::render();
 });
 
 $app->router->get('/contact', function () {
