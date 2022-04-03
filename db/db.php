54f2daf49a0ac55fa1f5af33cdbd157d310b2600
kazutotakeuchi-32 <kazutotakeuchi32@gmail.com>
2022-04-03 15:49:00 +0900

    db接続 成功

--- db/db.php
+++ db/db.php
@@ -1,16 +1,32 @@
 <?php 
+
+require 'vendor/autoload.php';
+
 class Db
 {
-
   public static function connect()
   {
-    $dsn = 'mysql:dbname=php_tr;host=localhost';
-    $user = 'root';
-    $password = 'yes';
-    $dbh = new PDO($dsn, $user, $password);
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__. "/..")->load();
+    $dsn = "mysql:dbname={$dotenv['DB_DATABASE']};host={$dotenv['DB_HOST']};";
+    $user = $dotenv["DB_USERNAME"];
+    $password = $dotenv["DB_PASSWORD"];
+    $options = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+    ];
+    $dbh = new PDO($dsn, $user, $password, $options);
     $dbh->query('SET NAMES utf8');
     return $dbh;
   }
-
 }
+
+
+$pdo = Db::connect();
+$sql = "SELECT * FROM users";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$users = $stmt->fetchAll();
+
+var_dump($users);
 
